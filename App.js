4cb149ee5de33ebcb21cc6c48f56fb95060c8530
Jorge Sandoval <j.sandovalsantibez@uandresbello.edu>
2023-01-19 13:20:09 -0300

    Se Agrega el Proyecto

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {TextInput} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>Login</Text>
+      <Text style={styles.sub_Titulo}>INICIA SESIÓN PARA CONTINUAR</Text>
+      <Text style={styles.descripcion}>INICIA SESIÓN PARA CONTINUAR</Text>
+      <TextInput
+        placeholder="email@ingelecsa.cl"
+        style={styles.textInput}
+      />
+      <TextInput
+        placeholder="clave"
+        style={styles.textInput}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f1f1f1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo:{
+    fontSize: 60,
+    color: '#34434D',
+    fontWeight: 'bold',
+  },
+  sub_Titulo:{
+    fontSize: 20,
+    color: 'gray',
+  },
+  descripcion:{
+    fontSize: 10,
+    color: '#34434D',
+    fontWeight: 'bold',
+  },
+  textInput:{
+    padding: 10,
+    paddingStart: 30,
+    width: '80%',
+    height: 50,
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: '#fff',
+  }
 });
