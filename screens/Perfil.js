cc21917ea621fc11ef86b7bb3b6625f62c4639f1
Javiera <jaavi0214@gmail.com>
2023-02-17 12:14:05 -0300

    Vista modificada perfil

--- screens/Perfil.js
+++ screens/Perfil.js
@@ -1,35 +1,42 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+
 
 
 const Perfil = () => (
-  <View
-  style={[
-    styles.container,
+
+  
+  <View style={[ styles.container,
     {
-      // Try setting `flexDirection` to `"row"`.
       flexDirection: 'column',
     },
   ]}>
-  <Text style={{
-    flex: 1, 
-    backgroundColor: '#545454',
-    fontSize: 40, 
-    justifyContent: 'center'
-  
-    }}>              Perfil</Text>
-  <View style={{flex: 2, backgroundColor: '#6D6D6D'}} />
-  <View style={{flex: 3, backgroundColor: '#A2A2A2'}} />
+
+  <Text style={{flex: 1.8, backgroundColor: '#465679', color: 'white', fontSize: 18, textAlign: 'right', textAlignVertical: 'bottom'}}>Nombre, apellido        </Text>
+  <Text style={{flex: 1.5, backgroundColor: '#E4E5E7', color: 'black', fontSize: 16, textAlign: 'right', textAlignVertical: 'top'}}>Nivel usuario                    </Text>
+  <View style={{flex: 3, backgroundColor: 'white'}} />
+
 </View>
-);
+
+)
+
+  
+
 
 const styles = StyleSheet.create({
   container: {
     flex:1,
+  },
+  buttonContainer:{
+    flex:2,
+    backgroundColor: '#E4E5E7',
   },
   nav: {
     flexDirection: "row",
     justifyContent: "space-around",
   },
 });
+
+
+
 
 export default Perfil;
